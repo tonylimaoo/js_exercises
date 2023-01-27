b82fdf01b816c8d21170be777a0c8f54f0d62b1a
tonylimaoo <110262447+tonylimaoo@users.noreply.github.com>
2023-01-27 17:10:25 -0300

    Set Interval inside

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-var previousCookies = document.cookie;
-var changeCookie = previousCookies;
-var myInterval = setInterval(() =>{
-    changeCookie = document.cookie
-    if(changeCookie !== previousCookies){
-        document.cookie = 'cookie5_afterCookieCreation='
-        clearInterval(myInterval)
-    }
-}, 1000)
+
 
 (function(){
+    
     document.cookie = 'cookie1=';
     document.cookie = 'cookie2=';
     document.cookie = 'cookie3=';
     document.cookie = 'cookie4=';
+    
+    var previousCookies = document.cookie;
+    var changeCookie = previousCookies;
+    var myInterval = setInterval(() =>{
+        changeCookie = document.cookie
+        if(changeCookie !== previousCookies){
+            document.cookie = 'cookie5_afterCookieCreation='
+            clearInterval(myInterval)
+        }
+    }, 1000)
+    
     myInterval()
+    
 })()
