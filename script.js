8765bfaebb22cb5b975ef77650abf7179e36cc21
tonylimaoo <110262447+tonylimaoo@users.noreply.github.com>
2023-01-27 17:04:39 -0300

    Cookie after cookie creation manually

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+var myInterval = setInterval(() =>{
+    changeCookie = document.cookie
+    if(changeCookie !== previousCookies){
+        document.cookie = 'cookie5_afterCookieCreation='
+        clearInterval(myInterval)
+    }
+}, 1000)
+
 (function(){
-    console.log('script chamado')
     document.cookie = 'cookie1=';
     document.cookie = 'cookie2=';
     document.cookie = 'cookie3=';
     document.cookie = 'cookie4=';
+    myInterval()
 })()
