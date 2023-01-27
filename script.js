1817e5ff8998598be5262d59f1a558372bf3f654
tonylimaoo <110262447+tonylimaoo@users.noreply.github.com>
2023-01-27 16:21:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 (function(){
     console.log('script chamado')
+    document.cookie = 'cookie1=';
+    document.cookie = 'cookie2=';
+    document.cookie = 'cookie3=';
+    document.cookie = 'cookie4=';
 })()
