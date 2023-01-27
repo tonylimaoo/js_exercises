3a338fb06f34529e39421fe2d6b03e3c04af7bc4
tonylimaoo <110262447+tonylimaoo@users.noreply.github.com>
2023-01-27 17:51:26 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,8 @@
 
 
-(function(){
-    
+(function(){    
     document.cookie = 'cookie1=';
     document.cookie = 'cookie2=';
     document.cookie = 'cookie3=';
     document.cookie = 'cookie4=';
-    
-    var previousCookies = document.cookie;
-    var changeCookie = previousCookies;
-    var myInterval = function(){
-        return setInterval(() =>{
-            changeCookie = document.cookie
-            if(changeCookie !== previousCookies){
-                document.cookie = 'cookie5_afterCookieCreation='
-                clearInterval(myInterval)
-            }
-        }, 1000)
-    }
-    
-    myInterval()
-    
 })()
